7464eb5597a8243950c726b3f170f3660b9ec9c8
Prince Gupta <princegupta444555@gmail.com>
2023-07-04 20:25:16 +0530

    remove the menudetail file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "./logo.png";
 import MenuIcon from "@mui/icons-material/Menu";
-import Menudetail from "./Menudetail";
 import Without from "./Without";
 
 function App() {
-  const [menu, setcount] = useState(false);
-  function give() {
-    setcount((menu) => !menu);
-  }
   return (
     <div>
       <div className="nav">
-        <MenuIcon className="menu" onClick={give}/>
+        <MenuIcon className="menu" />
         <img src={logo} alt="logo"></img>
         <span className="name">Keep</span>
       </div>
-      <div>{menu === true ? <Menudetail /> : <Without />}</div>
+      <div>
+        <Without />
+      </div>
     </div>
   );
 }
